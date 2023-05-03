b5092128c258f3c023edee0b30c21898fb6d49eb
Jonathan Woo <jwred5@msn.com>
2023-05-02 19:48:17 -0700

    Clean up imports

--- src/main/java/com/phredrobotics/PhredItems.java
+++ src/main/java/com/phredrobotics/PhredItems.java
@@ -1,10 +1,7 @@
 package com.phredrobotics;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
-import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
