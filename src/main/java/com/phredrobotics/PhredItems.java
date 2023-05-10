06311f989b5b719808e674d316243234958748ee
Jonathan Woo <jwred5@msn.com>
2023-05-09 20:26:47 -0700

    Phred Item 5 Recipe + Group
    Phred item 5

--- src/main/java/com/phredrobotics/PhredItems.java
+++ src/main/java/com/phredrobotics/PhredItems.java
@@ -1,17 +1,29 @@
 package com.phredrobotics;
 
+import com.phredrobotics.items.PhredItem5;
+
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class PhredItems {
-    public static final Item PHRED_ITEM_5 = new Item(new FabricItemSettings());
-
+    public static final Item PHRED_ITEM_5 = new PhredItem5(new FabricItemSettings().maxCount(5));
+    private static final ItemGroup PHRED_ITEM_GROUP = FabricItemGroup.builder(new Identifier("phred", "phred"))
+	.icon(() -> new ItemStack(PHRED_ITEM_5))
+    .displayName(Text.translatable("Phred"))
+	.build();
     public static void register(){
         
         Registry.register(Registries.ITEM, new Identifier("phred", "phred_item_5"), PHRED_ITEM_5);
-  
+        ItemGroupEvents.modifyEntriesEvent(PHRED_ITEM_GROUP).register(content -> {
+            content.add(PHRED_ITEM_5);
+        });
     }
 }
---
+++ src/main/java/com/phredrobotics/items/PhredItem5.java
@@ -0,0 +1,24 @@
+package com.phredrobotics.items;
+
+import java.util.List;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+
+public class PhredItem5 extends Item {
+ 
+    public PhredItem5(Settings settings) {
+        super(settings);
+    }
+    @Override
+public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+ 
+    // default white text
+    tooltip.add(Text.translatable("phred 5 item"));
+ 
+}
+}
