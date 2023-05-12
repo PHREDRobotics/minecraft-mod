056b966eb309757145d540e27a209b4869b07ebd
Llamalemwastaken <mcffbn@gmail.com>
2023-05-12 13:15:09 -0700

    Fixed a crash when putting on an elytra
    The vest now checks if the item is an elytra before asking what material the armor is. Also added name to Googly Eyes.

--- src/main/java/com/phredrobotics/items/PhredVest.java
+++ src/main/java/com/phredrobotics/items/PhredVest.java
@@ -14,6 +14,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 
@@ -73,8 +74,7 @@ if(hasCorrectArmorOn(mapArmorMaterial, player) && !hasPlayerEffect) {
 
 private boolean hasChestplateOn(PlayerEntity player) {
 ItemStack chestplate = player.getInventory().getArmorStack(2);
-
-return !chestplate.isEmpty();
+return !chestplate.isEmpty() && !chestplate.isOf(Items.ELYTRA);
 }
 
 private boolean hasCorrectArmorOn(ArmorMaterial material, PlayerEntity player) {
