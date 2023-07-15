573c7a16969548dc52ee89e0b6cb065ea3690a4a
Nate <nathaniel.houck@student.philomath.k12.or.us>
2023-07-15 15:56:18 -0700

    Added Robot Frame Class

---
+++ src/main/java/com/phredrobotics/items/PhredRobotFrame.java
@@ -0,0 +1,11 @@
+package com.phredrobotics.items;
+
+import net.minecraft.item.Item;
+
+public class PhredRobotFrame extends Item{
+   
+ 
+    public PhredRobotFrame(Settings settings) {
+    super(settings);
+    }
+}
