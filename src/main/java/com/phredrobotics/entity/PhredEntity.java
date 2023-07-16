85d867e974e1654d8ceea115306348ae7ff82ecc
mcffbn <mcffbn@gmail.com>
2023-07-16 15:57:06 -0700

    Make PHRED Rideable
    You can ride PHRED

--- src/main/java/com/phredrobotics/entity/PhredEntity.java
+++ src/main/java/com/phredrobotics/entity/PhredEntity.java
@@ -1,14 +1,25 @@
 package com.phredrobotics.entity;
 
+import javax.annotation.Nullable;
+
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 /*
@@ -18,12 +29,54 @@ import net.minecraft.world.World;
  * MobEntity has movement controls and AI capabilities.
  * PathAwareEntity has pathfinding favor and slightly tweaked leash behavior.
  */
-public class PhredEntity extends PathAwareEntity {
- 
-    public PhredEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
+public class PhredEntity extends AnimalEntity {
+	public PhredEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
+		this.setStepHeight(1);
+        this.ignoreCameraFrustum = true;
     }
+    // Let the player ride the entity
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+		if (!this.hasPassengers()) {
+			player.startRiding(this);
 
+			return super.interactMob(player, hand);
+		}
+
+		return super.interactMob(player, hand);
+	}
+    // Apply player-controlled movement
+    @Override
+	public void travel(Vec3d pos) {
+		if (this.isAlive()) {
+			if (this.hasPassengers()) {
+				LivingEntity passenger = (LivingEntity)getPrimaryPassenger();
+				this.prevYaw = getYaw();
+				this.prevPitch = getPitch();
+
+				setYaw(passenger.getYaw());
+				setPitch(passenger.getPitch() * 0.5f);
+				setRotation(getYaw(), getPitch());
+
+				this.bodyYaw = this.getYaw();
+				this.headYaw = this.bodyYaw;
+				float x = passenger.sidewaysSpeed * 0.5F;
+				float z = passenger.forwardSpeed;
+
+				if (z <= 0)
+					z *= 0.25f;
+
+				this.setMovementSpeed(0.3f);
+				super.travel(new Vec3d(x, pos.y, z));
+			} else {
+				super.travel(pos);
+			}
+		}
+	}
+    @Nullable
+	public LivingEntity getPrimaryPassenger() {
+		return getFirstPassenger() instanceof LivingEntity entity ? entity : null;
+	}
     public static final EntityType<PhredEntity> PHRED = Registry.register(
             Registries.ENTITY_TYPE,
             new Identifier("phred", "phred"),
@@ -33,6 +86,9 @@ public class PhredEntity extends PathAwareEntity {
 
         FabricDefaultAttributeRegistry.register(PHRED, PhredEntity.createMobAttributes());
     }
-
+	@Override
+	public PassiveEntity createChild(ServerWorld arg0, PassiveEntity arg1) {
+		return null;
+	}
 
 }
