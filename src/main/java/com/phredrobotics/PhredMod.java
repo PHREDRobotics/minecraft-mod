7f4eaff7bf3d98630f8ed00540ef603c9fe3954c
mcffbn <77407713+mcffbn@users.noreply.github.com>
2023-07-30 16:06:11 -0700

    Update PhredMod.java
    No asterisk

--- src/main/java/com/phredrobotics/PhredMod.java
+++ src/main/java/com/phredrobotics/PhredMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.phredrobotics.entity.*;
+import com.phredrobotics.entity.PhredEntity;
 
 public class PhredMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
