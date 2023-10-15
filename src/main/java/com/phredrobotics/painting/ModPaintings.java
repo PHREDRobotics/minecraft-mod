7f9c028ab309d9a3d44e73ef3d941efbbdaefab7
mcffbn <mcffbn@gmail.com>
2023-10-15 15:59:03 -0700

    fix paintings

--- src/main/java/com/phredrobotics/painting/ModPaintings.java
+++ src/main/java/com/phredrobotics/painting/ModPaintings.java
@@ -7,8 +7,8 @@ import net.minecraft.entity.decoration.painting.PaintingVariant;
 import net.minecraft.util.Identifier;
 
 public class ModPaintings {
-    public static final PaintingVariant vlc = registerPainting("vlc", new PaintingVariant(16, 16));
-    public static final PaintingVariant pingu = registerPainting("pingu", new PaintingVariant(64, 32));
+    public static final PaintingVariant VLC = registerPainting("vlc", new PaintingVariant(16, 16));
+    public static final PaintingVariant PINGU = registerPainting("pingu", new PaintingVariant(64, 32));
 
     private static PaintingVariant registerPainting(String name, PaintingVariant paintingVariant) {
         return Registry.register(Registries.PAINTING_VARIANT, new Identifier(PhredMod.MOD_ID, name), paintingVariant);
