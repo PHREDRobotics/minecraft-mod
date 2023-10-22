dc6e2e964a28c79b92dd5405e57068a927456d0f
Jonathan Woo <jwred5@msn.com>
2023-10-22 16:02:48 -0700

    Updated paths for minecraft files on server

--- server-files/minecraft-server-deploy.sh
+++ server-files/minecraft-server-deploy.sh
@@ -20,10 +20,13 @@ cp $SERVER_PATH/server.properties $BACKUP_PATH/server.properties
 mv $SERVER_PATH/mods/fabric-phred-mod*.jar $BACKUP_PATH/
 
 #Move files from new release
-mv $TMP_PATH/minecraft@.service /etc/systemd/system/minecraft@.service
-mv $TMP_PATH/ops.json $SERVER_PATH/ops.json
-mv $TMP_PATH/server.properties $SERVER_PATH/server.properties
-mv $TMP_PATH/fabric-phred-mod*.jar $SERVER_PATH/mods/
+mv $TMP_PATH/server-files/minecraft@.service /etc/systemd/system/minecraft@.service
+mv $TMP_PATH/server-files/ops.json $SERVER_PATH/ops.json
+mv $TMP_PATH/server-files/server.properties $SERVER_PATH/server.properties
+
+#Copy the mod jar, after deleting the source jar
+rm $TMP_PATH/distfiles/fabric-phred-mod-*-sources.jar
+mv $TMP_PATH/distfiles/fabric-phred-mod*.jar $SERVER_PATH/mods/
 
 #Start the server
 sudo systemctl start minecraft@$SERVERNAME
